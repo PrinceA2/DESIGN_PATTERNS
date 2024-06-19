9d433b9694adf785f4f9cf0042f5555600e4837f
Prince <tiwariprince938@gmail.com>
2024-06-19 17:20:50 +0530

    Singleton Design Pattern commited

---
+++ Design_Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Design_Pattern;
+
+Thread t1 = new Thread(() =>
+{
+    var instance = UploadServices.Instance(1);
+});
+
+Thread t2= new Thread(() =>
+{
+    var instance = UploadServices.Instance(2);
+});
+
+t1.Start();
+t2.Start();
+
+t1.Join();
+t2.Join();
---
+++ Design_Pattern/UploadServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    class UploadServices
+    {
+        private UploadServices(){}
+        public int UploadId { get; private set; }
+
+        private static UploadServices _instance;
+        private static readonly object _instanceLock = new object();
+        
+        public static UploadServices Instance(int id)
+        {
+
+            if(_instance == null)
+            {
+                lock(_instanceLock)
+                {
+                    if(_instance == null)
+                    {
+                        _instance = new UploadServices();
+                        _instance.UploadId = id;    
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
